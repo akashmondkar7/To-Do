e81bff206d41e5fc56e43108cd6357c6a5f55c8f
Your Name <akashmondkar03@gmail.com>
2026-06-03 23:15:18 +0530

    ejs file added

--- index.js
+++ index.js
@@ -4,7 +4,19 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
+app.set("view engine","ejs")
 
+app.get("/",(req,resp)=>{
+   resp.render("list")
+})
+
+app.get("/add",(req,resp)=>{
+   resp.render("add")
+})
+
+app.get("/update",(req,resp)=>{
+   resp.render("update")
+})
 const port = process.env.PORT;
 
 app.listen(port,()=>{
