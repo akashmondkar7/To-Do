172bda84a51aafe425d56595795a4adb32928fa4
Your Name <akashmondkar03@gmail.com>
2026-06-03 22:55:18 +0530

    server

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT;
+
+app.listen(PORT,()=>{
+    console.log(`Server Running on ${port}`)
+})
