f72b93e591ed918184bc320d37e6ea307bf908ab
Your Name <akashmondkar03@gmail.com>
2026-06-05 21:01:30 +0530

    MongoDb Connected

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import path from 'path'
+import path from 'path';
+import { MongoClient } from 'mongodb';
+import { url } from 'inspector';
 
 dotenv.config();
 
@@ -9,6 +11,14 @@ const publicPath= path.resolve('public')
 
 app.use(express.static(publicPath));
 app.set("view engine","ejs")
+
+
+const dbName="node-project";
+
+const collectionName="ToDoList";
+const url ="mongodb://localhost:27017"
+const client= new MongoClient(url)
+
 
 app.get("/",(req,resp)=>{
    resp.render("list")
